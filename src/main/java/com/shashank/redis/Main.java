562cdfb488120cd7a7754799c98b74838fe58384
Shashank <shashankpal.1909@gmail.com>
2024-03-19 14:33:07 +0530

    respond to ping

--- src/main/java/com/shashank/redis/Main.java
+++ src/main/java/com/shashank/redis/Main.java
@@ -1,19 +1,29 @@
 package com.shashank.redis;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Redis Starting Up...");
-		ServerSocket serverSocket = null;
+		ServerSocket serverSocket;
 		Socket clientSocket = null;
 		int port = 6379;
 		try {
 			serverSocket = new ServerSocket(port);
 			serverSocket.setReuseAddress(true);
 			clientSocket = serverSocket.accept();
+			
+			DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+			System.out.println("received byte = " + inputStream.readByte());
+			
+			OutputStream outputStream = clientSocket.getOutputStream();
+			outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+			
 		} catch (IOException e) {
 			System.out.println("IOException: " + e.getMessage());
 		} finally {
