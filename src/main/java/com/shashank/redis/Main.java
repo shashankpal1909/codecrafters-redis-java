6996aee44f49e5309de7382c44b2474b9e380abc
Shashank <shashankpal.1909@gmail.com>
2024-03-19 14:26:21 +0530

    initial setup

---
+++ src/main/java/com/shashank/redis/Main.java
@@ -0,0 +1,29 @@
+package com.shashank.redis;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Redis Starting Up...");
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+		int port = 6379;
+		try {
+			serverSocket = new ServerSocket(port);
+			serverSocket.setReuseAddress(true);
+			clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		} finally {
+			try {
+				if (clientSocket != null) {
+					clientSocket.close();
+				}
+			} catch (IOException e) {
+				System.out.println("IOException: " + e.getMessage());
+			}
+		}
+	}
+}
